5d61490b4aa065b4db79b4e91f6afd5150e885a2
Simon <simpla@ukr.net>
2022-01-28 22:38:45 +0200

    Home-work-6-correct

---
+++ HomeWork-Java-Petrik-Simon/src/homework6/Family.java
@@ -0,0 +1,94 @@
+package homework6;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class Family {
+    private Human mother;
+    private Human father;
+    private Human[] childrens;
+    private Pet pet;
+
+    public Family(Human mother, Human father, Human[] childrens) {
+        this.mother = mother;
+        this.father = father;
+        this.childrens = childrens;
+    }
+
+    public void addChildren(Human children){
+        Human[] humans = Arrays.copyOf(childrens, childrens.length + 1);
+        children.setFamily(this);
+        humans[childrens.length] = children;
+        childrens = humans;
+    }
+    public void deleteChild (int index){
+        if (index < 0 || index >= childrens.length) {
+            return;
+        }
+        Human[] humans = new Human[childrens.length-1];
+        System.arraycopy(childrens, 0, humans, 0, index);
+        System.arraycopy(childrens, index + 1, humans, index, childrens.length - index-1);
+        childrens = humans;
+    }
+
+    public Human getMother() {
+        return mother;
+    }
+
+    public void setMother(Human mother) {
+        this.mother = mother;
+    }
+
+    public Human getFather() {
+        return father;
+    }
+
+    public void setFather(Human father) {
+        this.father = father;
+    }
+
+    public Human[] getChildrens(Human sister) {
+        return childrens;
+    }
+
+    public void setChildrens(Human[] childrens) {
+        this.childrens = childrens;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Family family = (Family) o;
+        return Objects.equals(mother, family.mother) && Objects.equals(father, family.father) && Arrays.equals(childrens, family.childrens);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(mother, father);
+        result = 31 * result + Arrays.hashCode(childrens);
+        return result;
+    }
+    @Override
+    protected void finalize() {
+        System.out.println("Удаляемый объектв классе Family в методе finalize(): " + this);
+    }
+
+    @Override
+    public String toString() {
+        return "Family{" +
+                "mother=" + mother +
+                ", father=" + father +
+                ", children=" + Arrays.toString(childrens) +
+                ", pet=" + pet +
+                '}';
+    }
+}
---
+++ HomeWork-Java-Petrik-Simon/src/homework6/HumanTest.java
@@ -0,0 +1,15 @@
+package homework6;
+
+import org.junit.jupiter.api.Test;
+
+import static homework5.Human.countFamily;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class HumanTest {
+    @Test
+    void getCountFamily() {
+        Human mather = new Human("Olga", "Smirnova", 25);
+        Human father = new Human("Nikolay", "Smirnov", 29);
+       assertEquals(2, countFamily);
+    }
+}
